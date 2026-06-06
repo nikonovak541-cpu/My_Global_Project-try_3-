0b0feebf8f76836ace5956b967411ce75cb73996
Arthur <Mairse96@mail.ru>
2026-06-07 09:52:48 +1000

    All cart templates were included

---
+++ carts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def cart_add(request, prduct_id):
+    ...
+
+
+def cart_change(request, prduct_id):
+    ...
+
+
+def cart_remove(request, prduct_id):
+    ...
