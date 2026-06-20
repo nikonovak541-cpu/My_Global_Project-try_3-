12e8c2910f5293fa98c8a9df4d9c81884dc8cc06
Arthur <Mairse96@mail.ru>
2026-06-21 00:07:58 +1000

    Modal window and ajax final

--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -7,5 +7,5 @@ from carts.utils import get_user_carts
 register = template.Library() # Да с БОЛЬШОЙ БУКВЫ, БОЛЬШИМ РЕГИСТРОМ НАДО ПИСАТЬ ПЕРВУЮ БУКВУ В СЛОВЕ "Library"
 
 @register.simple_tag()
-def user_cart(request):
+def user_carts(request):
     return get_user_carts(request)
--- carts/views.py
+++ carts/views.py
@@ -23,7 +23,7 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1) # Если карт нет, то мы создаем их
 
-            
+
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
@@ -35,12 +35,64 @@ def cart_add(request):
     return JsonResponse(response_data)
 
 
-def cart_change(request, prduct_slug):
-    ...
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
 
+    cart = Cart.objects.get(id=cart_id)
 
-def cart_remove(request, cart_id):
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
     
+    
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity_delete": updated_quantity,
+    }
+    
+    return JsonResponse(response_data)
+
+
+def cart_remove(request):
+
+    cart_id = request.POST.get("cart_id")
     cart = Cart.objects.get(id=cart_id)
     cart.delete()
-    return redirect(request.META['HTTP_REFERER']) # С какой страницы пользователь попал, туда и возвращается
+
+    user_cart = get_user_carts(request)
+
+    # Вычисляем итоги здесь, чтобы быть уверенными
+    total_price = sum(item.product.sell_price() * item.quantity for item in user_cart)
+    total_quantity = sum(item.quantity for item in user_cart)
+
+    # Передаём их в контекст отдельно
+    context = {
+        "carts": user_cart,
+        "total_price": total_price,
+        "total_quantity": total_quantity,
+    }
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html",
+        context,
+        request=request
+    )
+
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": cart.quantity,
+        "total_price": float(total_price),
+        "total_quantity": total_quantity,
+    }
+    return JsonResponse(response_data)
+
+# -----------------------------
+    # УБИЛ ЗДЕСЬ ПОЧТИ 5 ДНЕЙ. НО МОДАЛЬНОЕ ОКНО ПЕРЕСТАЛО ПОКАЗЫВАТЬ (NAN) ТОЛЬКО КОГДА НАЧАЛ ИСПОЛЬЗОВАТЬ (CONTEXT) - РАБОЧИЙ СПОСОБ НАВЕРХУ!!!!!!!
+    # СПАСИБО ВЕЛИКОМУ GOOGLE И PYTHON DOCUMENTATION!! 
+# -----------------------------
